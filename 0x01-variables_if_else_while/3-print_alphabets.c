95bc86741148ae4f8846bf2ea8021ff6d0354ad5
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-11 20:28:49 +0000

    3,4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 65;
+	while (i <= 90)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
