0b1978591c85963874ebea3f83f939a67ed9c715
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-16 15:04:59 +0000

    4-6

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Detects if a character is alpha or not
+ * @c: Character to detect.
+ * Return: 1 if true, 0 if false.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _abs - Returns the absolute value of an integer
+ * @i: integer
+ * Return: absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
