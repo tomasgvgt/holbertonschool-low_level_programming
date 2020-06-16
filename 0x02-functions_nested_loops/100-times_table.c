ff929f7e54655c66559d9751e7df46fd87d466fb
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-16 22:41:08 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, num;
 
-	if (n >= 0 || n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -26,7 +26,7 @@ void print_times_table(int n)
 				else
 				{
 					_putchar(num / 100 + '0');
-					_putchar(num / 10 + '0');
+					_putchar((num / 10) % 10 + '0');
 					_putchar(num % 10 + '0');
 				}
 
