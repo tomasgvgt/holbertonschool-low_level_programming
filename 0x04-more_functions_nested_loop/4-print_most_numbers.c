ce6e394a1ce5b01515913637231771fbb381b4bc
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-18 16:46:36 +0000

    correction

--- 0x04-more_functions_nested_loop/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loop/4-print_most_numbers.c
@@ -7,7 +7,7 @@
 void print_most_numbers(void)
 {
 	char num;
-	for (num = 48; num >= 57; num++)
+	for (num = 48; num <= 57; num++)
 	{
 		if (num != 50 && num != 52)
 			_putchar(num);
--- 0x04-more_functions_nested_loop/5-more_numbers.c
+++ 0x04-more_functions_nested_loop/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void);
 
 	for (i = 0; i < 10; i++)
 	{
-		for (num = 0; num >= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
 				_putchar(num / 10 + '0');
