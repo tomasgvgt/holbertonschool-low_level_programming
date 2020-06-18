f0df6d67ddc8001d53b80218ba8a7fc1f912ff09
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-18 16:55:05 +0000

    correction

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - Prints '_' n numerber of times
+ * @n: number of times ti print '_'
+ */
+
+void print_line(int n)
+{
+	char l;
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
