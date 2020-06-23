736441ca0f8fb9e172fe73925b6969d328fc5051
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-23 20:03:55 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,14 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i] != '\0')
-		i++;
-	while (i >= 0)
-		_putchar(s[i--]);
-	_putchar('\n');
+while (s[i] != '\0')
+i++;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
 }
