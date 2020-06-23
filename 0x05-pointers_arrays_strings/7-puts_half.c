dca9d0216dd53388b3c94ca5a84f4aa2c5d433c2
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-23 16:33:41 +0000

    7 corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	for (i = (len - 1) / 2 + 1; s[i] != '\0'; i++)
+	for (i = (len - 1) / 2 + 1; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
