2743921eec7429bd1d2fb9e6c762f76e71225cac
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-25 23:32:36 +0000

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,8 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 		for (j = 0; sep[j] != '\0'; j++)
 		{
 			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
