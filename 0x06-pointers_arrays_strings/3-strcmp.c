eb058631cfd004af00bb3851a3a1a4d194950aa9
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-25 20:39:06 +0000

    3 betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * _strcmp - compares two strings, man strcmp.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: s1 - s2.
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	return (*s1 - *s2);
