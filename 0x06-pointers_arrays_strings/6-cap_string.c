61add50b1c928f1aee0d2bb36fa39f26767c5eca
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-25 23:27:09 +0000

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
+		}
+	}
+	return (s);
+}
