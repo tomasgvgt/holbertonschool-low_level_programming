742e3ca67a8d43c9afde1fe37341f332aa91e10d
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-26 00:00:27 +0000

    8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * rot13 - codes a string to rot13 code
+ * @s: string
+ * Return: pointer to converted string.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char ch[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; ch[j] != '\0'; j++)
+		{
+			if (s[i] == ch[j])
+				s[i] = rot13[j];
+		}
+	}
+	return (s);
+}
