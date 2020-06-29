a023f51f84f84336a2e833690e456700bb355128
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-29 23:00:36 +0000

    3 correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,7 +19,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				count++, break;
+			{
+				count++;
+				break;
+			}
 			else
 				break;
 		}
