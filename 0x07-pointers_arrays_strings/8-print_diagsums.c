63535c9556504f944f012077c53d238ab4e2e632
tomasgvgt <tomasgvg.t@gmail.com>
2020-06-30 00:15:22 +0000

    Commented original code for reference

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,14 +12,15 @@ void print_diagsums(int *a, int size)
 {
 	int len = size * size, sum1 = 0, sum2 = 0, i;
 
-	/**for (i = 0; i < len; i++)
-	{
-		if (i == 0 || i % (size + 1) == 0)
-			sum1 += a[i];
-		if (i % (size - 1) == 0 && i <= len - size)
-			sum2 += a[i];
-	}
-	*/
+	/**
+	 * for (i = 0; i < len; i++)
+	 *{
+	 *	if (i == 0 || i % (size + 1) == 0)
+	 *		sum1 += a[i];
+	 *	if (i % (size - 1) == 0 && i <= len - size)
+	 *		sum2 += a[i];
+	 *}
+	 */
 	for (i = 0; i < len; i += size + 1)
 	{
 		sum1 += a[i];
