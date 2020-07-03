c4d5f2fe1e37ba1df8c74f6b67c1aac517e0770a
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-03 01:38:29 +0000

    100 v2

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
+/**
+ * wildcmp - function that compares two strings.
+ * s2 can contain the special character * that can replace
+ * any string (including an empty string).
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: 1 if identical, 0 if not.
+ */
+
 int wildcmp(char *s1, char *s2)
 {
-	if ((*s1 == *s2 || *s2 == 42) && (*(s1 + 1) == '\0' && *(s2 + 1) == '\0'))
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if ((*s1 != *s2) && (*s2 != 42))
-		return (0);
-	if (*s2 == 42 && (*(s1 + 1) != '\0') && (*(s1 + 1) != *(s2 + 1)))
-		return (wildcmp(s1 + 1, s2));
-	return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	return (0);
 }
