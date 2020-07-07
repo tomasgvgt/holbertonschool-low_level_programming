5f70d75ba23b4d4d933a09771d2ac5f599176de4
tomasgvgt <tomasgvgt@gmail.com>
2020-07-07 22:36:03 +0000

    1 checking if checker likes it

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,15 +12,15 @@ char *_strdup(char *str)
 	char *s;
 	unsigned int size = 0, i = 0;
 
-	if (str == '\0')
-		return ('\0');
+	if (str == NULL)
+		return (str);
 
 	while (str[size] != '\0')
 		size++;
 
 	s = malloc(1 + size * sizeof(char));
-	if (s == '\0')
-		return ('\0');
+	if (s == NULL)
+		return (s);
 
 	i = 0;
 	while (i < size)
