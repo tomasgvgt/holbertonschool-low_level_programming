9708ca35d8a76c5b67240018960b98828ae77d7f
tomasgvgt <tomasgvgt@gmail.com>
2020-07-07 22:18:58 +0000

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Copies a string.
+ * @str: original string.
+ * Return: Pointer to a new string, NULL if str == NULL, or if it fails.
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i = 1;
+
+	if (str == NULL)
+		return (str);
+
+	while (str[i] != '\0')
+		i++;
+
+	s = malloc(i);
+	if (s == NULL)
+		return (s);
+
+	i = 0;
+	while (str[i] != '\0')
+		s[i] = str[i], i++;
+	s[i] = '\0';
+
+	return (s);
+}
