3e388095a948baf8655881e52077f4874edc1c0c
tomasgvgt <tomasgvgt@gmail.com>
2020-07-08 00:27:04 +0000

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid  - Frees the memory of a 2d array.
+ * @grid: pointer to the 2d array.
+ * @height: height of array.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
