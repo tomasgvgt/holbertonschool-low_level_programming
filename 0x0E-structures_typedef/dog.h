23a0c29a75c30bab60341b5b6aa5fd89e6f4b02b
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-14 00:08:57 +0000

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - tructure of a dog.
+ * @dog: pointer.
+ * @age: age of dog.
+ * @owner: pointer to the ouner name.
+ */
+
 struct dog
 {
 	char *dog;
 	float age;
 	char *owner;
 }
+
+#endif
