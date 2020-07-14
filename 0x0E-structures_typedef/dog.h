61e3aae6467597111e390186e0d26c780c9df678
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-14 16:52:33 +0000

    typedef experiment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,12 @@
  * @owner: pointer to the ouner name.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
