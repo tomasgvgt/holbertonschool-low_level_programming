3493a8e88c034afa2e1325feb7efbf0920adf9d5
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-14 01:40:25 +0000

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees  dogs.
+ * @d:  pointer to dog_t structure.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
