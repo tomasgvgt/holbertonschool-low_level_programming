102ab691710dff19472106db3848698a38831c58
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-16 14:22:12 +0000

    bettying the experiment

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	/*if (argv[2] != "+" && argv[2] != "-" && argv[2] != "*" && argv[2] != "/" && argv[2] != "%")
-	if (get_op_func() == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	*/
+	/**
+	 * if (*argv[2] != '+' && *argv[2] != '-'
+	 *	&& *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+	 *	{
+	 *	printf("Error\n");
+	 *	exit(99);
+	 *	}
+	 */
 
 	a = atoi(argv[1]), b = atoi(argv[3]);
 
