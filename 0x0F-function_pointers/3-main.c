1bd73d4193fa1f309cba7ed913e584df6622e518
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-16 14:10:19 +0000

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+	/*if (argv[2] != "+" && argv[2] != "-" && argv[2] != "*" && argv[2] != "/" && argv[2] != "%")*/
+	if (argv[2] == NULL)
 	{
 		printf("Error\n");
 		exit(99);
