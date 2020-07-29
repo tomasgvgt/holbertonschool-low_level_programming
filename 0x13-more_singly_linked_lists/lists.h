0a0be2748f7f2b1237b26c8b195abb8bc419c6ba
tomasgvgt <tomasgvg.t@gmail.com>
2020-07-29 22:08:27 +0000

    header actualization

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+const listint_t *check_if_loop(const listint_t *h);
+const listint_t *start_of_loop(const listint_t *p1, const listint_t *h);
 
 #endif
