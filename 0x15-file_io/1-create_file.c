163dfa810253fb9ab2afbe995b9d41aaecf87815
tomasgvgt <tomasgvg.t@gmail.com>
2020-08-04 20:39:18 +0000

    1 experiment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (text_content == 0)
-		return (1);
 	/*open file, create if it doesnt exist, truncate it if already exists*/
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == 0)
+		return (1);
 	/*lenght of text_content*/
 	for (i = 0; text_content[i]; i++)
 		;
