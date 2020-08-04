785d5e8171da3e0591b9e25eebcd2d05fc8fd014
tomasgvgt <tomasgvg.t@gmail.com>
2020-08-04 22:44:51 +0000

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - Copies the content of a file to anotherone
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 1 succes.
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, rd, wr, cl1, cl2;
+	char *buf;
+
+	if (argc != 3)
+		write(2, "Usage: cp file_from file_to\n", 29), exit(97);
+	if (!argv[1])
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd1 = open(argv[1], O_RDONLY);
+	buf = malloc(1024);
+	rd = read(fd1, buf, 1024);
+	if (rd == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), free(buf), exit(98);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	wr = write(fd2, buf, rd);
+	if (wr == -1)
+		dprintf(2, "Can't write to %s", argv[2]), free(buf), exit(99);
+	free(buf);
+	cl1 = close(fd1);
+	if (cl1 == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd1), exit(100);
+	cl2 = close(fd2);
+	if (cl2 == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd2), exit(100);
+	return (1);
+}
