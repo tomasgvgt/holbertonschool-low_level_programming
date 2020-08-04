20fcda1761615c3a4b692fa820831431b153e90c
tomasgvgt <tomasgvg.t@gmail.com>
2020-08-04 20:31:50 +0000

    1 experiment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,10 @@ int create_file(const char *filename, char *text_content)
 	/*write text content to the file*/
 	wr = write(fd, text_content, i);
 	if (wr == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
