18f08c204468fa648a28fb0ae6e5f87318a7e773
tomasgvgt <tomasgvg.t@gmail.com>
2020-08-04 23:42:36 +0000

    1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,7 +34,7 @@ size_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	/*write into stdout*/
-	bytes_written = write(1, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1)
 	{
 		free(buffer);
