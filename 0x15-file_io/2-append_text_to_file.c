4206de99c3269d5a101818eb78878179fca27d6d
tomasgvgt <tomasgvg.t@gmail.com>
2020-08-04 20:42:59 +0000

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,6 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 	/*lenght of text_content*/
 	for (i = 0; text_content[i]; i++)
 		;
