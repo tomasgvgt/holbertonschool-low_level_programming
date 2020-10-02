5dc453b861f92ca27b6d1cb0bbd2bc083e97c964
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-02 23:35:24 +0000

    1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function that implements the djb2 algorithm.
+ * @str: Key of string type.
+ * Return: Created hash for that key.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
