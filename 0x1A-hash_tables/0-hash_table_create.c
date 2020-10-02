db314f0126a057665ddea6dfdfed97450b2a65d7
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-02 23:27:51 +0000

    0 minor change

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	table = malloc(sizeof(hash_table_t *));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
