c04226cd39d86cb9771d91383aa224396f78c411
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-03 02:47:59 +0000

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL, *tmp = NULL;
+	hash_node_t *tmp = NULL, *new_node = NULL;
 
-	if (!ht || !key || !value)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index]; /*set tmp to index in the array*/
-	while (tmp) /*search if there is a key equal to the one given*/
+	tmp = ht->array[index];
+	while (tmp)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
@@ -29,7 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	/*If there was not key equal to the one given, create and set new node*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
@@ -42,11 +41,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	if (!new_node->value)
 	{
-		free(new_node->key), free(new_node);
+		free(new_node->key);
+		free(new_node);
 		return (0);
 	}
-	tmp = ht->array[index]; /*PLace node at begining of the list*/
-	new_node->next = tmp;
-	tmp = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
