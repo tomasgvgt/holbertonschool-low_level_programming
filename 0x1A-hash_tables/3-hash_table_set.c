d96c811052cea175bfbf9be42f66a90a159c78f7
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-03 01:44:03 +0000

    3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets key: value pairs to the hash table.
+ * @ht: hash table.
+ * @key: key.
+ * @value: value.
+ * Return: 1 succes, 0 fail.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = NULL, *tmp = NULL;
+
+	if (!ht | !key | !value)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	/*set tmp to index in the array*/
+	tmp = ht->array[index];
+	/*search if there is a key equal to the one given*/
+	while (tmp)
+	{
+		if (strcmp(tmp->key, (char *)key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	/*If there was not key equal to the one given, create and set new node*/
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node);
+		return (0);
+	}
+	/*PLace new_node at the begining of the list of the index*/
+	tmp = ht->array[index];
+	new_node->next = tmp;
+	tmp = new_node;
+	return (1);
+}
